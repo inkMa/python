0b6159466fdb3c53fbc5b0632f6bbc3f097eb1ef
max-MB <2248926678@qq.com>
2018-10-01 22:56:02 -0700

    hello,world

---
+++ hello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+import sys
+
+
+def sys_test():
+	print(sys.argv)
+
+
+
+
+if __name__ == '__main__' :
+	print('hello,world')
+	sys_test()
+else :
+	print('')
